dd9995057d71e520be4b0dd735e4751b84010634
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-24 23:26:27 +0100

    Update files

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function Phone(brand, price, color) {
 
 Phone.prototype.printInfo = function() {
 	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
-}
+};
 
 var iPhone6S = new Phone('Apple', 2250, 'silver');
 var samsungGalaxyS6 = new Phone('Samsung', 1500, 'gold');
